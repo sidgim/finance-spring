29d0e47dd40ad690dde0cbf3e1b2f270bdba2488
Gastón <29836462+sidgim@users.noreply.github.com>
2025-07-14 22:24:51 -0400

    feat: New generic response utils

---
+++ commons/src/main/java/com/glara/springcloud/commons/exceptions/DuplicateResourceException.java
@@ -0,0 +1,26 @@
+package com.glara.springcloud.commons.exceptions;
+
+/**
+ * Exception thrown when there's a conflict with an existing resource
+ */
+public class DuplicateResourceException extends BaseException {
+    private static final String ERROR_CODE_PREFIX = "DUP";
+
+    public DuplicateResourceException(String resourceType, String field, String value) {
+        super(
+            String.format("%s with %s '%s' already exists", resourceType, field, value),
+            ERROR_CODE_PREFIX + "-001",
+            String.format("Attempted to create a %s with a %s that is already in use", resourceType.toLowerCase(), field)
+        );
+    }
+
+    // Specific constructor for user email conflict
+    public static DuplicateResourceException emailConflict(String email) {
+        return new DuplicateResourceException("User", "email", email);
+    }
+
+    // Specific constructor for username conflict
+    public static DuplicateResourceException usernameConflict(String username) {
+        return new DuplicateResourceException("User", "username", username);
+    }
+}
